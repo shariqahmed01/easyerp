bb49fed1daa2d6d7eb4a615c86f9ff51af25dcb9
Shariq Ahmed <67710319+shariqahmed01@users.noreply.github.com>
2022-04-19 12:32:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 1500, 2000, 8);
+    counter("count2", 0, 250, 8);
+
+});
